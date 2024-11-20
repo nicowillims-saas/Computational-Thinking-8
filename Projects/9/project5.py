2cfb70af389cc03b5350387f74f89095aa6c38f5
nicowillims-saas <nicowilliams@seattleacademy.org>
2024-11-20 22:10:38 +0000

    test

--- Projects/9/project5.py
+++ Projects/9/project5.py
@@ -2,10 +2,11 @@
 import codesters, random
 from codesters import StageClass
 stage = StageClass()
+stage.set_background("project5bg.jpg")
 
 
-player = codesters.Sprite("gene.png")
-player.set_size(0.2)
+player = codesters.Sprite("gingerbread.png")
+player.set_size(0.5)
 player.go_to(0,-200)
 stage.disable_floor()
 
@@ -21,9 +22,9 @@ def falling_object():
     global gameOver
     if not gameOver:
         x_position = random.randint(-250,250)
-        object = codesters.Sprite("soccerball", x_position, 250)
-        object.set_size(0.4)
-        object.set_y_speed(-10)
+        object = codesters.Sprite("present", x_position, 250)
+        object.set_size(0.1)
+        object.set_y_speed(-5)
    
 stage.event_interval(falling_object, 0.5)
 
@@ -31,11 +32,11 @@ stage.event_interval(falling_object, 0.5)
 # Section 3 - Collision
 
 
-def collision(s1, s2):
+def collision(, s2):
     global lives, gameOver
    
-    if s2.get_image_name() == "soccerball":
-        stage.remove_sprite(s2)
+    if s2.get_image_name() == "present.png":
+        stage.remove_sprite("s2")
         if lives == 0:
             print("test")
         else:
@@ -47,3 +48,9 @@ player.event_collision(collision)
 
 
 # Section 4 - Controls
+def move_left(sprite):
+    sprite.move_left (4)
+def move_right(sprite):
+    sprite.move_right (4)
+player.event_key("a", move_left)
+player.event_key("d", move_right)
